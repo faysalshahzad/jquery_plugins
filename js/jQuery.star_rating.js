b68095e42be72724b8a2cce35f8161b7ab39a616
f.shahzad <faysalshahzad1@gmail.com>
2014-06-05 16:20:36 +0200

    jquery plugin project created and restructured.

---
+++ js/jQuery.star_rating.js
@@ -0,0 +1,12 @@
+/**
+ * @author Faysal Shahzad
+ */
+
+( function($) {
+		$.fn.starRating = function(opts) {
+			
+}(jQuery));
+
+$('.star-rating').starRating({
+	serviceURL : "/service/Rating/rate"
+});
